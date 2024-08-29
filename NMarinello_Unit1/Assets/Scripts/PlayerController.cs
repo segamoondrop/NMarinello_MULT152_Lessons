56cbc21fe70bad87ac557f8fb9d8f2be8c53a2fa
segamoondrop <audeonico443@gmail.com>
2024-08-29 15:38:09 -0700

    Finished Lesson 1C

--- NMarinello_Unit1/Assets/Scripts/PlayerController.cs
+++ NMarinello_Unit1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 15.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,6 @@ public class PlayerController : MonoBehaviour
         Debug.Log(Time.deltaTime);
         // Vector3.forward -- > (0, 0 ,1)
         // (0, 0, 1) * Time.deltaTime = (0, 0, .016) * = (0, 0, .16)
-        transform.Translate(Vector3.forward * Time.deltaTime * 15);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
