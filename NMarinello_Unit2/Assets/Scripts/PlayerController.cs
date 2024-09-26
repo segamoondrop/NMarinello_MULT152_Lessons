e0cf8d4e92dd0b55d30c48e65c907075b7f92fb3
segamoondrop <audeonico443@gmail.com>
2024-09-26 02:30:21 -0700

    Finished Lesson2B

--- NMarinello_Unit2/Assets/Scripts/PlayerController.cs
+++ NMarinello_Unit2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     private float xRange = 20.0f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +28,11 @@ public class PlayerController : MonoBehaviour
         if(transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
