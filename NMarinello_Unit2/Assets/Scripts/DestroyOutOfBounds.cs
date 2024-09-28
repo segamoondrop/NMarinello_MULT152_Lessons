6874ac54c02d7583a4d9aef0930a0375adade4b7
segamoondrop <audeonico443@gmail.com>
2024-09-27 20:44:19 -0700

    Finished Lesson 2D

--- NMarinello_Unit2/Assets/Scripts/DestroyOutOfBounds.cs
+++ NMarinello_Unit2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -22,6 +22,8 @@ public class DestroyOutOfBounds : MonoBehaviour
         else if(transform.position.z < bottomOfScene)
         {
             Destroy(gameObject);
+            Debug.Log("Game Over!");
         }
+
     }
 }
--- NMarinello_Unit2/Assets/Scripts/SpawnManager.cs
+++ NMarinello_Unit2/Assets/Scripts/SpawnManager.cs
@@ -9,18 +9,20 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", 3.0f, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            float randXPos = Random.Range(-xPosRange, xPosRange);
-            int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 randPos = new Vector3(randXPos, 0, 21);
-            Instantiate(animalPrefabs[animalPrefabIndex],new Vector3(randXPos, 0, 21), animalPrefabs[animalPrefabIndex].transform.rotation);
-        }
+
+    }
+
+    void SpawnRandomAnimal()
+    {
+        float randXPos = Random.Range(-xPosRange, xPosRange);
+        int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 randPos = new Vector3(randXPos, 0, 21);
+        Instantiate(animalPrefabs[animalPrefabIndex],new Vector3(randXPos, 0, 21), animalPrefabs[animalPrefabIndex].transform.rotation); 
     }
 }
